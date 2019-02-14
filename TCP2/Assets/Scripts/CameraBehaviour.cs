d36af066a4ccb74857765e1b7ad8b8b24efa32af
Juliana Potengy <julianalpotengy@gmail.com>
2019-02-14 18:27:59 -0200

    Movimentação da câmera e do player

---
+++ TCP2/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        mouseX = 1, mouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.mouseX;
+
+    private float minimumVert = -45.0f;
+    private float maximumVert = 45.0f;
+    private float senseHorizontal = 10.0f;
+    private float senseVertical = 10.0f;
+
+    private float rotationX = 0;
+	
+	void Update ()
+    {
+		if(axes == RotationAxis.mouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * senseHorizontal, 0);
+        }
+        else if(axes == RotationAxis.mouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * senseVertical;
+            rotationX = Mathf.Clamp(rotationX, minimumVert, maximumVert);
+
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+	}
+}
---
+++ TCP2/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private float speed = 6.0f;
+    private float gravity = -9.8f;
+    private CharacterController charCont;
+    
+	void Start ()
+    {
+        charCont = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaY = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaY);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charCont.Move(movement);
+    }
+}
